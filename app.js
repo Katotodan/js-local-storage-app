020784cac74ae4b19c01b93bfc47fe3efc9f91d8
Katotodan <katotodan@gmail.com>
2022-06-08 17:54:40 +0530

    button for removal added

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const nameP = document.querySelector("#nameInp");
 const phoneP = document.querySelector("#teleNumber");
 const countryP = document.querySelector("#country");
-const aside = document.querySelector("aside")
+const aside = document.querySelector("aside");
+const hide = document.querySelector(".clean")
+
 
 const sub = document.querySelector("button");
 
 let personContainer = JSON.parse(localStorage.getItem("person")) ? JSON.parse(localStorage.getItem("person")) : [] ;
 
 if(localStorage.length <1){
-    aside.style.display = "none"
+    aside.style.display = "none";
+    hide.style.display = "none";
 }
 const addPerson = () =>{
     for(let i =0; i<personContainer.length; i++){
@@ -17,8 +20,11 @@ const addPerson = () =>{
         const pName = document.createElement("p");
         const pPhone = document.createElement("p");
         const pCountry = document.createElement("p");
-        container.append(title, pName, pPhone, pCountry);
+        const deleteperson = document.createElement("output");
+        deleteperson.innerHTML = `❌`
+        container.append(title, pName, pPhone, pCountry , deleteperson);
         aside.appendChild(container);
+        container.style.position = "relative"
 
         title.innerHTML = `Persone number: ${i+1}`;
         pName.innerHTML = personContainer[i].name;
@@ -38,4 +44,28 @@ sub.addEventListener("click" ,(e) =>{
     localStorage.setItem("person", JSON.stringify(personContainer));
     location.reload()
     
+})
+const deleteP = document.querySelectorAll("aside output");
+
+for(let i=0; i<deleteP.length; i++){
+    deleteP[i].addEventListener("click" , () =>{
+        let newarr = [];
+        for(let j=0; j<personContainer.length; j++){
+            if(j != i){
+                newarr.push(personContainer[j])
+            }
+        }
+        personContainer = []
+        localStorage.clear()
+        personContainer = [...newarr]
+        if(personContainer.length >= 1){
+            localStorage.setItem("person", JSON.stringify(personContainer));
+        }
+        location.reload()
+    })
+}
+hide.addEventListener("click" , () =>{
+    personContainer = [];
+    localStorage.clear();
+    location.reload()
 })
